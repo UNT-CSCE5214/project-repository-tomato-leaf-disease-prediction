a7c0ec675a0e8b30049efc4c073e90a8d397894e
RishikaNamineni <79569514+RishikaNamineni@users.noreply.github.com>
2022-11-19 15:59:55 -0600

    Update lr.py
    Create a custom Keras callback to continue and optionally set LR or halt training

--- lr.py
+++ lr.py
@@ -49,3 +49,66 @@ class LR_ASK(keras.callbacks.Callback):
         
         
         
+        def on_epoch_end(self, epoch, logs=None):  # method runs on the end of each epoch
+        vloss=logs.get('val_loss')  # get the validation loss for this epoch
+        aloss=logs.get('loss')
+        if epoch >0:
+            deltav = self.lowest_vloss- vloss 
+            pimprov=(deltav/self.lowest_vloss) * 100 
+            self.plist.append(pimprov)
+            deltaa=self.lowest_aloss-aloss
+            aimprov=(deltaa/self.lowest_aloss) * 100
+            self.alist.append(aimprov)
+        else:
+            pimprov=0.0 
+            aimprov=0.0
+        if vloss< self.lowest_vloss:
+            self.lowest_vloss=vloss
+            self.best_weights=self.model.get_weights() # set best weights to model's initial weights
+            self.best_epoch=epoch + 1            
+            msg=f'\n validation loss of {vloss:7.4f} is {pimprov:7.4f} % below lowest loss, saving weights from epoch {str(epoch + 1):3s} as best weights'
+            print_in_color(msg, (0,255,0)) # green foreground
+        else: # validation loss increased
+            pimprov=abs(pimprov)
+            msg=f'\n validation loss of {vloss:7.4f} is {pimprov:7.4f} % above lowest loss of {self.lowest_vloss:7.4f} keeping weights from epoch {str(self.best_epoch)} as best weights'
+            print_in_color(msg, (255,255,0)) # yellow foreground
+            if self.dwell: # if dwell is True when the validation loss increases the learning rate is automatically reduced and model weights are set to best weights
+                lr=float(tf.keras.backend.get_value(self.model.optimizer.lr)) # get the current learning rate
+                new_lr=lr * self.factor
+                msg=f'learning rate was automatically adjusted from {lr:8.6f} to {new_lr:8.6f}, model weights set to best weights'
+                print_in_color(msg) # cyan foreground
+                tf.keras.backend.set_value(self.model.optimizer.lr, new_lr) # set the learning rate in the optimizer
+                model.set_weights(self.best_weights) # set the weights of the model to the best weights      
+                
+        if aloss< self.lowest_aloss:
+            self.lowest_aloss=aloss        
+        if self.ask: # are the conditions right to query the user?
+            if epoch + 1 ==self.ask_epoch: # is this epoch the one for quering the user?
+                msg='\n Enter H to end training or  an integer for the number of additional epochs to run then ask again'
+                print_in_color(msg) # cyan foreground
+                ans=input()
+                
+                if ans == 'H' or ans =='h' or ans == '0': # quit training for these conditions
+                    msg=f'you entered {ans},  Training halted on epoch {epoch+1} due to user input\n'
+                    print_in_color(msg)
+                    self.model.stop_training = True # halt training
+                else: # user wants to continue training
+                    self.ask_epoch += int(ans)
+                    if self.ask_epoch > self.epochs:
+                        print('\nYou specified maximum epochs of as ', self.epochs, ' cannot train for ', self.ask_epoch, flush =True)
+                    else:
+                        msg=f'you entered {ans} Training will continue to epoch {self.ask_epoch}'
+                        print_in_color(msg) # cyan foreground
+                        if self.dwell==False:
+                            lr=float(tf.keras.backend.get_value(self.model.optimizer.lr)) # get the current learning rate
+                            msg=f'current LR is  {lr:8.6f}  hit enter to keep  this LR or enter a new LR'
+                            print_in_color(msg) # cyan foreground
+                            ans=input(' ')
+                            if ans =='':
+                                msg=f'keeping current LR of {lr:7.5f}'
+                                print_in_color(msg) # cyan foreground
+                            else:
+                                new_lr=float(ans)
+                                tf.keras.backend.set_value(self.model.optimizer.lr, new_lr) # set the learning rate in the optimizer
+                                msg=f' changing LR to {ans}'
+                                print_in_color(msg) # cyan foreground
